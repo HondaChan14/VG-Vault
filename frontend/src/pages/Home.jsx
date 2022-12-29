f857c4d05e7dda508ebe4bb0ce01f175227ecb4d
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2022-12-28 23:48:24 -0500

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import NavBar from './components/NavBar/NavBar';
+import Hero from './components/Hero';
+import Footer from './components/Footer/Footer';
+import '../index.css'
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "background": "#252121",
+        "navbtn": "#6B399D",
+      }
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#7a85cc",
+        
+"secondary": "#28ccbe",
+        
+"accent": "#ea3c9f",
+        
+"neutral": "#2F2D39",
+        
+"base-100": "#3A3A3B",
+        
+"info": "#ABDBE8",
+        
+"success": "#2FD0A5",
+        
+"warning": "#F5AA14",
+        
+"error": "#EB1E36",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
